ad8008936fdfadb3790271ac870883b580d485f0
5881311885 <Listuffq@gmail.com>
2025-09-26 19:16:56 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/particles.js"></script>
+<script>
+particlesJS('background', {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    move: { speed: 1 }
+  }
+});
+</script>
